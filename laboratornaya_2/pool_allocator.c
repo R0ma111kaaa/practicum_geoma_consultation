98cbb1839f6f334305ec9e20f6bdf429743cdffb
Vladislav-Ushakov-FIT-241 <chesmotrishotvernis@gmail.com>
2025-06-03 10:35:58 +0600

    format files

--- laboratornaya_2/pool_allocator.c
+++ laboratornaya_2/pool_allocator.c
@@ -3,54 +3,56 @@
 #include <stdlib.h>
 #include <string.h>
 
-PoolAllocator* pool_allocator_create(size_t block_size, size_t block_count) {
-    if (block_size < sizeof(void*)) {
-        block_size = sizeof(void*);
-    }
+PoolAllocator *pool_allocator_create(size_t block_size, size_t block_count) {
+  if (block_size < sizeof(void *)) {
+    block_size = sizeof(void *);
+  }
 
-    PoolAllocator* allocator = (PoolAllocator*)malloc(sizeof(PoolAllocator));
-    if (!allocator) return NULL;
+  PoolAllocator *allocator = (PoolAllocator *)malloc(sizeof(PoolAllocator));
+  if (!allocator)
+    return NULL;
 
-    allocator->memory = malloc(block_size * block_count);
-    if (!allocator->memory) {
-        free(allocator);
-        return NULL;
-    }
+  allocator->memory = malloc(block_size * block_count);
+  if (!allocator->memory) {
+    free(allocator);
+    return NULL;
+  }
 
-    allocator->block_size = block_size;
-    allocator->block_count = block_count;
-    allocator->free_list = allocator->memory;
+  allocator->block_size = block_size;
+  allocator->block_count = block_count;
+  allocator->free_list = allocator->memory;
 
-    char* current = (char*)allocator->memory;
-    for (size_t i = 0; i < block_count - 1; ++i) {
-        *(void**)current = current + block_size;
-        current += block_size;
-    }
-    *(void**)current = NULL;
+  char *current = (char *)allocator->memory;
+  for (size_t i = 0; i < block_count - 1; ++i) {
+    *(void **)current = current + block_size;
+    current += block_size;
+  }
+  *(void **)current = NULL;
 
-    return allocator;
+  return allocator;
 }
 
-void* pool_alloc(void* allocator_ptr) {
-    PoolAllocator* allocator = (PoolAllocator*)allocator_ptr;
+void *pool_alloc(void *allocator_ptr) {
+  PoolAllocator *allocator = (PoolAllocator *)allocator_ptr;
 
-    if (!allocator->free_list) return NULL;
+  if (!allocator->free_list)
+    return NULL;
 
-    void* block = allocator->free_list;
-    allocator->free_list = *(void**)block;
-    return block;
+  void *block = allocator->free_list;
+  allocator->free_list = *(void **)block;
+  return block;
 }
 
-void pool_free(void* allocator_ptr, void* ptr) {
-    PoolAllocator* allocator = (PoolAllocator*)allocator_ptr;
+void pool_free(void *allocator_ptr, void *ptr) {
+  PoolAllocator *allocator = (PoolAllocator *)allocator_ptr;
 
-    *(void**)ptr = allocator->free_list;
-    allocator->free_list = ptr;
+  *(void **)ptr = allocator->free_list;
+  allocator->free_list = ptr;
 }
 
-void pool_allocator_destroy(PoolAllocator* allocator) {
-    if (allocator) {
-        free(allocator->memory);
-        free(allocator);
-    }
+void pool_allocator_destroy(PoolAllocator *allocator) {
+  if (allocator) {
+    free(allocator->memory);
+    free(allocator);
+  }
 }
--- laboratornaya_2/pool_allocator.h
+++ laboratornaya_2/pool_allocator.h
@@ -1,13 +1,13 @@
 #include <stddef.h>
 
 typedef struct PoolAllocator {
-    void* memory;
-    void* free_list;
-    size_t block_size;
-    size_t block_count;
+  void *memory;
+  void *free_list;
+  size_t block_size;
+  size_t block_count;
 } PoolAllocator;
 
-PoolAllocator* pool_allocator_create(size_t block_size, size_t block_count);
-void* pool_alloc(void* allocator);
-void pool_free(void* allocator, void* ptr);
-void pool_allocator_destroy(PoolAllocator* allocator);
+PoolAllocator *pool_allocator_create(size_t block_size, size_t block_count);
+void *pool_alloc(void *allocator);
+void pool_free(void *allocator, void *ptr);
+void pool_allocator_destroy(PoolAllocator *allocator);
