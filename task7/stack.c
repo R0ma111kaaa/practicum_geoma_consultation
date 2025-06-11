239d1f1cc89dd9a575178ce01caa3b7c7643d032
qwerty-mod <slavaynk@mail.ru>
2025-06-11 15:02:53 +0600

    доавил уведомление о переполнении стека

--- task7/stack.c
+++ task7/stack.c
@@ -13,10 +13,12 @@ int is_empty(Stack *stack) { return stack->top == -1; }
 
 int is_full(Stack *stack) { return stack->top == stack->size - 1; }
 
-void push(Stack *stack, int value) {
-  if (!is_full(stack)) {
-    stack->arr[++stack->top] = value;
+bool push(Stack *stack, int value) {
+  if (is_full(stack)) {
+    return false;
   }
+  stack->arr[++stack->top] = value;
+  return true;
 }
 
 bool pop(Stack *stack, int *outValue) {
